9f458353d436e8fb4d73df06746741471e2234be
Ivy <ivy@onprem.com>
2022-02-15 17:05:46 -0600

    updated assignment

--- unit-12-1.js
+++ unit-12-1.js
@@ -32,3 +32,26 @@ class Motorcycle extends Vehicle {
 		return 'VROOM!!!'
 	}
 }
+
+// Part Four
+class Garage {
+	constructor (capacLimit) {
+		this.vehicles = []
+		this.capac = 0
+		this.capacLimit = capacLimit
+	}
+	add (vehicle) {
+		const {vehicles, capacLimit} = this
+		let {capac} = this
+		if (vehicle instanceof Vehicle) {
+			if (capac < capacLimit) {
+				vehicles.push(vehicle)
+				this.capac++ //why won't using capac++ work here? Wouldn't capac === this.capac?
+				return 'Vehicle added!'
+			}
+			return 'Sorry, we are full.'
+		} else {
+			return 'Only vehicles are allowed in here!'
+		}
+	}
+}
